5f09151a18b6047221e09a78c40280f023ce3545
Veramanda <faidatun237@gmail.com>
2023-11-27 22:17:05 +0700

    menambahkan modal dan button

--- src/components/dashboard/buttons/AddDataButton.jsx
+++ src/components/dashboard/buttons/AddDataButton.jsx
@@ -1,7 +1,10 @@
-export default function AddDataButton(){
-  return(
-    <button  className="w-full bg-[#1E3465] px-4 py-3.5 text-white rounded-xl text-sm min-[840px]:text-base">
-      Add Data
+export default function AddDataButton({ handleAddModal }) {
+  return (
+    <button
+      onClick={handleAddModal}
+      className="font-semibold w-full bg-[#1E3465] px-4 py-3.5 text-white rounded-xl text-sm min-[840px]:text-base"
+    >
+      Tambah Data
     </button>
-  )
+  );
 }
--- src/components/dashboard/modals/EditModal.jsx
+++ src/components/dashboard/modals/EditModal.jsx
@@ -1,30 +1,133 @@
-export default function EditModal(){
-  return(
+import { useState } from "react";
+
+export default function EditModal({ nutrition_record, handleEditModal }) {
+  const [editNutritionRecordFormData, setEditNutritionRecordFormData] =
+    useState({
+      weight: nutrition_record.weight,
+      height: nutrition_record.height,
+      head_circumference: nutrition_record.head_circumference,
+      growth_date: nutrition_record.growth_date,
+    });
+
+  const handleInputChange = (event) => {
+    setEditNutritionRecordFormData({
+      ...editNutritionRecordFormData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const fetchEditNutritionRecord = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/nutrition-records/${nutrition_record.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(editNutritionRecordFormData),
+        }
+      );
+
+      if (!response.ok) {
+        return;
+      }
+
+      const data = await response.json();
+      console.log(data);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetchEditNutritionRecord();
+  };
+
+  return (
     <>
-      <div id="edit_modal_container" className="fixed top-0 left-0 w-full h-screen bg-black/50" style={{display: "none"}}>
+      <div className="fixed top-0 left-0 w-full h-screen bg-black/50 z-[99]">
         <div className="flex w-full h-full justify-center items-center px-6">
-          <form id="edit_modal_form" className="relative flex flex-col w-full max-w-[600px] py-[26px] px-[18px] min-[840px]:px-[26px] bg-white rounded-xl gap-y-6 min-[840px]:gap-y-8">
+          <form
+            onSubmit={submit}
+            className="relative flex flex-col w-full max-w-[600px] py-[26px] px-[18px] min-[840px]:px-[26px] bg-white rounded-xl gap-y-6 min-[840px]:gap-y-8"
+          >
             <div className="text-start min-[840px]:text-center">
-              <h1 className="font-bold text-[22px] min-[840px]:text-[24px] text-[#272C49]">Edit Data Form</h1>
-              <p className="text-xs min-[840px]:text-sm text-[#898989]">Edit data anak anda</p>
+              <h1 className="font-bold text-[22px] min-[840px]:text-[24px] text-[#272C49]">
+                Edit Data Form
+              </h1>
+              <p className="text-xs min-[840px]:text-sm text-[#898989]">
+                Edit data anak anda
+              </p>
             </div>
             <div className="flex flex-col gap-y-5">
-              <input type="date" id="edit_tanggal_pertumbuhan" className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg" required></input>
-              <input type="text" id="edit_berat" className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg" pattern="^[0-9]*\.?[0-9]*$" placeholder="Berat Badan cth:(2.55)" required></input>
-              <input type="text" id="edit_tinggi" className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg" pattern="^[0-9]*\.?[0-9]*$" placeholder="Tinggi Badan cth:(6.2)" required></input>
-              <input type="text" id="edit_lingkar_kepala" className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg" pattern="^[0-9]*\.?[0-9]*$" placeholder="Lingkar Kepala cth:(30.212)" required></input>
+              <input
+                type="date"
+                name="growth_date"
+                className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg"
+                value={editNutritionRecordFormData.growth_date}
+                onChange={handleInputChange}
+                required
+              ></input>
+              <input
+                type="text"
+                name="weight"
+                className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg"
+                pattern="^[0-9]*\.?[0-9]*$"
+                placeholder="Berat Badan cth:(2.55)"
+                value={editNutritionRecordFormData.weight}
+                onChange={handleInputChange}
+                required
+              ></input>
+              <input
+                type="text"
+                name="height"
+                className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg"
+                pattern="^[0-9]*\.?[0-9]*$"
+                placeholder="Tinggi Badan cth:(30)"
+                value={editNutritionRecordFormData.height}
+                onChange={handleInputChange}
+                required
+              ></input>
+              <input
+                type="text"
+                name="head_circumference"
+                className="text-sm min-[840px]:text-base px-3 py-2 focus:outline-none border border-[#D1D9E2] text-[#474F7C] placeholder:text-[#898989] rounded-lg"
+                pattern="^[0-9]*\.?[0-9]*$"
+                placeholder="Lingkar Kepala cth:(30.212)"
+                value={editNutritionRecordFormData.head_circumference}
+                onChange={handleInputChange}
+                required
+              ></input>
             </div>
-            <button type="submit" className="w-full bg-[#1E3465] px-2 py-2.5 text-white rounded-lg text-sm min-[840px]:text-base">
+            <button
+              type="submit"
+              className="font-semibold w-full bg-[#1E3465] px-2 py-2.5 text-white rounded-lg text-sm min-[840px]:text-base"
+            >
               Edit
             </button>
 
-            <button id="close_edit_modal" type="button" className="bg-[#1E3465] px-1.5 min-[840px]:px-2 py-1 absolute top-4 right-4 min-[840px]:right-6 cursor-pointer rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 min-[840px]:w-5 fill-white" viewBox="0 0 384 512">
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+            <button
+              onClick={handleEditModal}
+              type="button"
+              className="bg-[#1E3465] px-1.5 min-[840px]:px-2 py-1 absolute top-4 right-4 min-[840px]:right-6 cursor-pointer rounded-full"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 min-[840px]:w-5 fill-white"
+                viewBox="0 0 384 512"
+              >
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+              </svg>
             </button>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }

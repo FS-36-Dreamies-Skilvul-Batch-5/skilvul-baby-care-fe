758043b079f6d3c7eeee004b527ca19417e0bae6
Bradley <bradleyandrewercole28@gmail.com>
2023-11-28 13:18:26 +0700

    Menggunakan Dashboard Loading Comp

--- src/routes/dashboardFE/Main.jsx
+++ src/routes/dashboardFE/Main.jsx
@@ -8,6 +8,7 @@ import NutritionStatusCard from "../../components/dashboard/cards/NutritionStatu
 import FirstAddModal from "../../components/dashboard/modals/FirstAddModal";
 import DailyNeedList from "../../components/dashboard/lists/DailyNeedList";
 import ImmunizationReminderList from "../../components/dashboard/lists/ImmunizationReminderList";
+import DashboardLoading from "../../components/dashboard/loadings/DashboardLoading";
 
 export default function Main() {
   const [isLoading, setIsLoading] = useState(true);
@@ -176,17 +177,7 @@ export default function Main() {
           </>
         ))}
 
-      <div
-        className={`${
-          isLoading ? "visible opacity-100" : "invisible opacity-0"
-        } fixed top-0 left-0 w-full h-screen bg-white flex justify-center items-center duration-[300ms] ease-out`}
-      >
-        <img
-          src="/assets/icons/only-baby-logo.png"
-          alt="BabyCare Logo"
-          className="w-16 h-16 sm:w-20 sm:h-20 animate-bounce"
-        />
-      </div>
+      <DashboardLoading loadState={isLoading}/>
     </>
   );
 }

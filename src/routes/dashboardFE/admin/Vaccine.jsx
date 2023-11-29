6215d06da41c5aef067e8d47d5ebb6f19787f238
Bradley <bradleyandrewercole28@gmail.com>
2023-11-29 20:33:56 +0700

    Menambahkan Headings untuk Admin Routes

--- src/routes/dashboardFE/admin/Vaccine.jsx
+++ src/routes/dashboardFE/admin/Vaccine.jsx
@@ -1,4 +1,5 @@
 import AdminSidebar from "../../../layouts/AdminSidebar";
+import Heading from "../../../components/dashboard/headings/Heading";
 import { checkUserRole } from "../../../utils/auth/checkUserRole";
 
 export default function Vaccine(){
@@ -7,6 +8,17 @@ export default function Vaccine(){
   return(
     <>
       <AdminSidebar/>
+
+      <div className="min-[840px]:pl-[62px] pt-12 min-[840px]:pt-0 pb-0">
+        <div className="flex flex-col w-full h-screen py-10 px-[20px] sm:px-[35px] min-[840px]:pl-[55px] gap-y-6 overflow-y-hidden">
+          <div className="w-full flex flex-col text-start mb-2">
+            <Heading
+              text="Vaksin"
+              ket="Berikut merupakan list semua vaksin"
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }

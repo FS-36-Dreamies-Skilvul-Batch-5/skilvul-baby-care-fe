70df14bd66e23459e65541370e335eacd431a80a
Bradley <bradleyandrewercole28@gmail.com>
2023-12-04 20:29:01 +0700

    Add Loading State for Actions

--- src/components/dashboard/admin/modals/EditArticleModal.jsx
+++ src/components/dashboard/admin/modals/EditArticleModal.jsx
@@ -11,6 +11,7 @@ export default function EditArticleModal({ articleInfo, handleEditModal }) {
     posted_on: articleInfo.posted_on,
   });
   const [articleCategories, setArticleCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const editorRef = useRef(null);
 
@@ -86,6 +87,7 @@ export default function EditArticleModal({ articleInfo, handleEditModal }) {
   };
 
   const submit = (e) => {
+    setIsLoading(true);
     e.preventDefault();
     fetchEditArticle();
   };
@@ -188,12 +190,24 @@ export default function EditArticleModal({ articleInfo, handleEditModal }) {
                   ></input>
                 </div>
 
-                <button
-                  type="submit"
-                  className="font-semibold w-full bg-[#1E3465] px-2 py-2.5 text-white rounded-lg text-sm min-[840px]:text-base"
-                >
-                  Edit Artikel
-                </button>
+                {!isLoading ? (
+                  <button
+                    type="submit"
+                    className="font-semibold w-full bg-[#1E3465] px-2 py-2.5 text-white rounded-lg text-sm min-[840px]:text-base"
+                  >
+                    Edit Artikel
+                  </button>
+                ) : (
+                  <div className="w-full flex justify-center items-center bg-[#1E3465] px-2 py-3 text-white rounded-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 fill-white animate-spin"
+                      viewBox="0 0 512 512"
+                    >
+                      <path d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8C121.8 95.6 64 169.1 64 256c0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1c-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256c0 141.4-114.6 256-256 256S0 397.4 0 256C0 140 77.1 42.1 182.9 10.6c16.9-5 34.8 4.6 39.8 21.5z" />
+                    </svg>
+                  </div>
+                )}
               </div>
             </div>
 

7692e451d18d7f9b360b963bcfbe748181c801ab
Bradley <bradleyandrewercole28@gmail.com>
2023-12-05 10:40:46 +0700

    Memperbaiki Blue Background

--- src/routes/auth/RegisterSuccess.jsx
+++ src/routes/auth/RegisterSuccess.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import BlueBackground from "../../assets/blue-background.webp"
 
 export default function RegisterSuccess() {
   return (
     <div className="
-      bg-[url('assets/images/blue-background.webp')] 
       bg-no-repeat bg-center bg-cover 
       w-full h-auto lg:h-screen
       flex justify-center items-center 
       px-4 py-20"
+      style={{
+        background: `url(${BlueBackground})`,
+      }}
     >
       <div className="flex flex-col bg-white justify-center items-center w-full max-w-[1064px] h-auto rounded-[24px] md:rounded-[40px] py-[130px] md:py-[102px] lg:py-[114px] px-3 gap-y-4 select-none">
         <div className="w-full lg:w-auto flex-auto lg:flex-1 flex justify-center items-center self-center">
